597f89ca602d2a90962d725b743c080a44034f16
Visheshank <Er.Visheshank.Mishra@Gmail.Com>
2025-10-27 23:18:01 -0500

    1. Optimised code structure.

--- api_main/utils/mistral_helper.py
+++ api_main/utils/mistral_helper.py
@@ -4,19 +4,22 @@ MISTRAL_API_KEY = "8okHgUSaSySgcorxhNR0KRV9g8j1Z099"  # Todo: put this in .env o
 
 client = Mistral(api_key=MISTRAL_API_KEY)
 
-def get_embeddings_from_str_list(text_chunks_list):
-    if not text_chunks_list:
+
+def _get_embeddings(inputs: list[str]) -> list[list[float]]:
+    if not inputs:
         return []
-
     try:
-        response = client.embeddings.create(
-            model="mistral-embed",  # todo: try out diff models, of diff size.
-            inputs=text_chunks_list
-        )
-
-        embeddings = [data.embedding for data in response.data] # Extracting just embedding vectors from the response.
-        return embeddings
-
+        response = client.embeddings.create(model="mistral-embed", inputs=inputs)
+        return [data.embedding for data in response.data]
     except Exception as e:
         print(f"Error calling Mistral API: {e}")
         return []
+
+
+def get_embeddings_from_str_list(text_chunks_list: list[str]) -> list[list[float]]:
+    return _get_embeddings(text_chunks_list)
+
+
+def get_embedding_from_str(query_text: str) -> list[float]:
+    embeddings = _get_embeddings([query_text])
+    return embeddings[0] if embeddings else []
