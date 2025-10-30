975ff85a5efcd22e2f54a0e103f4f498a9500752
Visheshank <Er.Visheshank.Mishra@Gmail.Com>
2025-10-30 03:16:19 -0500

    1. Self Code Review, changed code because 'None' can cause errors here, strip method does not work on none type.

--- api_main/utils/pdf_helper.py
+++ api_main/utils/pdf_helper.py
@@ -7,8 +7,8 @@ def extract_text_from_pdf(file_path: str) -> str:
     try:
         with pdfplumber.open(file_path) as pdf:
             for page in pdf.pages:
-                page_text = page.extract_text().strip()
-                if page_text.strip():
+                page_text = page.extract_text()
+                if page_text:
                     full_text += page_text + "\n"
         return full_text
     except Exception:
